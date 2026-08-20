e17a603a44223bef7176bb5171c26f2bdfd7f266
riles21 <riles21@gmail.com>
2026-08-19 19:52:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,235 @@
 /*
   McHenry Cobras team configuration
 
-  To add a team:
-    1. Copy one team object.
-    2. Give it a unique key.
-    3. Set active: true.
-    4. Enter the GameChanger Team ID.
-    5. Enter the GameChanger Schedule Widget ID when available.
-    6. Enter the head coach and up to 3 assistant coaches.
-    7. Enter a Facebook URL, or leave it blank.
+  Add/update teams here. The master index.html does not need to change.
 
-  To temporarily remove a team from use:
-    Set active: false.
+  Required:
+    active
+    name
+    season
+    gameChangerTeamId
+    gameChangerWidgetId
+
+  Coaching:
+    headCoach
+    assistantCoaches: up to 3 names
+
+  Facebook:
+    facebookUrl: leave "" until a team page is available.
 */
 
 const COBRAS_TEAMS = {
 
+  deubel8u: {
+    active: true,
+    name: "8U Deubel",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Mike Deubel",
+    assistantCoaches: [
+      "Ryan Freund",
+      "Chris Bigall",
+      "JR Shaver"
+    ],
+    facebookUrl: ""
+  },
+
+  mcandrews9u: {
+    active: true,
+    name: "9U McAndrews",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Justin McAndrews",
+    assistantCoaches: [
+      "Matt Rocha",
+      "Mike Prigge",
+      "Craig Almquist"
+    ],
+    facebookUrl: ""
+  },
+
+  hansen10u: {
+    active: true,
+    name: "10U Hansen",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Zach Hansen",
+    assistantCoaches: [
+      "James Wood",
+      "Pat Smith",
+      "Danny Beck"
+    ],
+    facebookUrl: ""
+  },
+
+  decicco10u: {
+    active: true,
+    name: "10U DeCicco",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Vince DeCicco",
+    assistantCoaches: [
+      "Jim Gosnell",
+      "Kevin Kocha",
+      "Jeremy Kiernicki"
+    ],
+    facebookUrl: ""
+  },
+
+  lennon11u: {
+    active: true,
+    name: "11U Lennon",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Travis Lennon",
+    assistantCoaches: [
+      "Cliff Anderson",
+      "Joey Jesky",
+      "Brian Ecklund"
+    ],
+    facebookUrl: ""
+  },
+
+  bozer11u: {
+    active: true,
+    name: "11U Bozer",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Ben Bozer",
+    assistantCoaches: [
+      "Mike Minnery",
+      "Tyler Well"
+    ],
+    facebookUrl: ""
+  },
+
+  meraz12u: {
+    active: true,
+    name: "12U Meraz",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Freddy Meraz",
+    assistantCoaches: [
+      "Conner McLean",
+      "Paul Lacson",
+      "Andrew Ressler"
+    ],
+    facebookUrl: ""
+  },
+
+  lorenz12u: {
+    active: true,
+    name: "12U Lorenz",
+    season: "2027 Season",
+    gameChangerTeamId: "jWdOuqoTjKow",
+    gameChangerWidgetId: "",
+    headCoach: "John Lorenz",
+    assistantCoaches: [
+      "Randy Ozimek",
+      "Nick Valenziano"
+    ],
+    facebookUrl: ""
+  },
+
+  baillargeon13u: {
+    active: true,
+    name: "13U Baillargeon",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Eric Baillargeon",
+    assistantCoaches: [
+      "Zach Hansen",
+      "Cory Lusk",
+      "Bart Harris"
+    ],
+    facebookUrl: ""
+  },
+
+  thiemeyer13u: {
+    active: true,
+    name: "13U Thiemeyer",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Joshua Thiemeyer",
+    assistantCoaches: [
+      "Donovan Christman",
+      "Jon Clements",
+      "Mike Pankiewicz"
+    ],
+    facebookUrl: ""
+  },
+
+  boettcher14u: {
+    active: true,
+    name: "14U Boettcher",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Aaron Boettcher",
+    assistantCoaches: [
+      "Nick Valenziano",
+      "Dan Pagels"
+    ],
+    facebookUrl: ""
+  },
+
+  dean15u: {
+    active: true,
+    name: "15U Dean",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Jeremy Dean",
+    assistantCoaches: [
+      "Dan Huels",
+      "Shane Frederick",
+      "Pat Lucas"
+    ],
+    facebookUrl: ""
+  },
+
+  bivona16u: {
+    active: true,
+    name: "16U Bivona",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Mike Bivona",
+    assistantCoaches: [
+      "Eric McCann",
+      "John Weber",
+      "Justin Lehr"
+    ],
+    facebookUrl: ""
+  },
+
+  deubel17u: {
+    active: true,
+    name: "17U Deubel",
+    season: "2027 Season",
+    gameChangerTeamId: "",
+    gameChangerWidgetId: "",
+    headCoach: "Mike Deubel",
+    assistantCoaches: [
+      "Blake Thome",
+      "Cooper Cohn"
+    ],
+    facebookUrl: ""
+  },
+
+  /*
+    Former team — kept here for reference.
+    Set to false so it does not appear as an active team.
+  */
   riley17u: {
     active: false,
     name: "17U Riley",
